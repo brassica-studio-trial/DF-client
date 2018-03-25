c82ef3c5997d4c2a16b87a088081417bd83e22d5
tetsuya-kudo-bs <tetsuya.kudo.bs@gmail.com>
2018-03-26 02:30:23 +0900

    no message

---
+++ client/Assets/Scripts/TowerLeft.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerLeft : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var parent = GameObject.Find("TowerLeft/animeTest(Clone)") as GameObject;
+
+        if (parent != null)
+        {
+            // 子のTransformを取り出す
+            var transforms = parent.GetComponentsInChildren<Transform>();
+
+
+            float posX = transform.position.x;
+            float posY = transform.position.y;
+            float posZ = transform.position.z;
+            posX++;
+            transform.position.Set(posX, posY, posZ);
+
+        }
+
+
+    }
+}
