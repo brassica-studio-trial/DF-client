2581cb1b0ef2948901f900b2664f70e5543b06a8
tetsuya-kudo-bs <tetsuya.kudo.bs@gmail.com>
2018-04-01 15:23:53 +0900

    enemyを作成

---
+++ client/Assets/Scripts/Button2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button2 : MonoBehaviour
+{
+    /// ボタンをクリックした時の処理
+    public void OnClick()
+    {
+        Debug.Log("Button click!");
+        // キャラのプレファブを取得
+        GameObject charaTest = (GameObject)Resources.Load("Prefabs/enemy1");
+
+        // 生成先のオブジェクトを取得
+        GameObject TowerLeft = GameObject.Find("TowerLeft");
+
+        // キャラ開始ポジションを設定
+        Vector3 position = new Vector3(-440, -520, 480);
+
+        // プレファブからインスタンス生成
+        //GameObjectのTowerLeftの子として生成
+        Instantiate(charaTest, position, Quaternion.identity, TowerLeft.transform);
+    }
+}
